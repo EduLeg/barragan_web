32ad6343081ce1b91f85832306c39dbedb82aaf7
Pablito0717 <129913885+Pablito0717@users.noreply.github.com>
2023-04-04 19:48:50 -0600

    API CHANGE

--- components/Googlemaps.js
+++ components/Googlemaps.js
@@ -37,7 +37,7 @@ const Mark4 = {
 function MyComponent() {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY2
     })
 
     const [map, setMap] = React.useState(null)
