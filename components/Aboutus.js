3aa2ce0efcf85f84b3d1850a061f08a6d4607c2e
Pablito0717 <129913885+Pablito0717@users.noreply.github.com>
2023-04-19 19:37:53 -0600

    Cambion en mision

--- components/Aboutus.js
+++ components/Aboutus.js
@@ -29,7 +29,7 @@ export default function Aboutus() {
                                         <HiOutlineBadgeCheck className="text-[3rem] text-white" />
                                     </div>
                                     <p className='text-2xl lg:text-4xl mb-3 font-bold text-black'>Misión</p>
-                                    <p>Producir, comercializar y distribuir alimentos al mayoreo y menudeo de primera calidad. Con el compromiso de satisfacer las necesidades de nuestros clientes y las de sus familias con el mejor servicio y atención.</p>
+                                    <p>Comercializar y distribuir alimentos al mayoreo y menudeo de primera calidad. Con el compromiso de satisfacer las necesidades de nuestros clientes y las de sus familias con el mejor servicio y atención.</p>
                                 </div>
                                 <div className="flex flex-col ">
                                     <div className="h-full w-[3.5rem] rounded-lg bg-[#ba2024]  flex items-center justify-center text-xl">
